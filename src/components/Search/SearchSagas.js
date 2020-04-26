fc0544d393be1efab821f35eedf5aa2960a491de
Nataliia Artiles Astelarra <natalia.artiles.astelarra@gmail.com>
2020-04-26 22:11:45 +0300

    fix cancel behavior

--- src/components/Search/SearchSagas.js
+++ src/components/Search/SearchSagas.js
@@ -21,42 +21,28 @@ import { getSearchQuery } from "./SearchReducer";
 
 function* search(action) {
   try {
-    //
-    // while (true) {
     const query = yield select(getSearchQuery);
-    console.log("start search saga", query);
-    // yield put(actions.requestStart())
-    yield delay(3000);
+    yield delay(2000);
     let searchResult = yield call(searchRepositories, query);
     yield put({
       type: SEARCH_SUCCESS,
       searchResult: searchResult.data.items,
     });
-    console.log("saga success---------");
-    //
+  } catch (err) {
+    console.log(err);
   } finally {
-    console.log("saga finally------");
     if (yield cancelled()) {
-      console.log("--cancelled--");
       yield put({ type: SEARCH_CANCELLED });
     }
   }
 }
 
+export function* handleSearch() {
+  const searchWatcher = yield fork(search);
+  yield take(CANCEL_SEARCH_REQUEST);
+  yield cancel(searchWatcher);
+}
+
 export function* onSearch() {
-  console.log("onSearch");
-  //   const searchWatcher = yield takeLatest(SEARCH_REQUEST, search);
-  //   yield take(CANCEL_SEARCH_REQUEST);
-  //   yield cancel(searchWatcher);
-
-  while (yield take(SEARCH_REQUEST)) {
-    // starts the task in the background
-    const searchWatcher = yield fork(search);
-
-    // wait for the user stop action
-    yield take(CANCEL_SEARCH_REQUEST);
-    // user clicked stop. cancel the background task
-    // this will cause the forked bgSync task to jump into its finally block
-    yield cancel(searchWatcher);
-  }
+  yield takeLatest(SEARCH_REQUEST, handleSearch);
 }
